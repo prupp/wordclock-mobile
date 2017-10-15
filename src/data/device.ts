e70505325874cb886762ca885c7ef5c2b6f4b97b
Philipp Rupp <philipp.rupp@deimos.ch>
2017-10-15 19:59:20 +0200

    add device list template

---
+++ src/data/device.ts
@@ -0,0 +1,6 @@
+export interface device {
+    icon: string;
+    name: string;
+    description?: string;
+    online: boolean;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { device } from '../../data/device';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  private devices: Array<device> = [
+    {
+      icon: '/assets/img/wc-thumb.png',
+      name: 'Wordclock',
+      description: 'lorem ipsum dolor sit amet',
+      online: true
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openSetupModal() {
+    
   }
 
 }
