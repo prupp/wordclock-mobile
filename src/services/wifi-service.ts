214f26c6a4cbb2644b7d1569c733540557165396
Philipp Rupp <rupp.philipp@gmail.com>
2017-10-22 02:02:49 +0200

    switch to wifiwizard cordova plugin, use hotspot plugin as fallback

--- src/services/wifi-service.ts
+++ src/services/wifi-service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hotspot, HotspotNetwork } from '@ionic-native/hotspot';
 
+declare let WifiWizard: any;
+
 @Injectable()
 export class WifiService {
 
@@ -11,8 +13,25 @@ export class WifiService {
    * Scans for nearby wifi networks
    * @return {Promise<HotspotNetwork[]>}
    */
-  scan(): Promise<HotspotNetwork[]> {
-    return this.hotspot.scanWifi();
+  scan(): Promise<any> {
+    if (WifiWizard) {
+      return new Promise((resolve, reject) => {
+        WifiWizard.startScan(data => {
+          setTimeout(
+            WifiWizard.getScanResults(data => {
+              resolve(data);
+            }, error => {
+              reject(error);
+            }),
+            3000);
+        }, error => {
+          reject(error);
+        });
+      });
+    } else {
+      // use hotspot plugin as fallback if WifiWizard isn't available
+      return this.hotspot.scanWifi();
+    }
   }
 
   /**
@@ -20,8 +39,19 @@ export class WifiService {
    * @param {string} ssid - wifi name 
    * @param {string=''} password
    */
-  connect(ssid: string, password: string = ''): Promise<void> {
-    return this.hotspot.connectToWifi(ssid, password);
+  connect(ssid: string, password: string = ''): Promise<any> {
+    if (WifiWizard) {
+      return new Promise((resolve, reject) => {
+        WifiWizard.connectNetwork(ssid, data => {
+          resolve(data);
+        }, error => {
+          reject(error);
+        });
+      });
+    } else {
+      // use hotspot plugin as fallback if WifiWizard isn't available
+      return this.hotspot.connectToWifi(ssid, password);
+    }
   }
 
 }
